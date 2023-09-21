cd36a90a647405475e350e2d82dffc72f69d9fd3
hajerkefi <Elkefihajer6@gmail.com>
2023-09-21 13:10:43 +0200

    algo checkpoint

---
+++ checkpoint.js
@@ -0,0 +1,19 @@
+c=0;
+v=0;
+
+ch=prompt("enter your sentence here")
+for (let i = 0; i < ch.length;i++) {
+    if(ch[i]==" "){
+        c++;
+    }
+   
+    if(ch[i]==="a"||ch[i]==="y"||ch[i]==="o"||ch[i]==="e"||ch[i]==="i") {
+        v++;
+    }
+    if(["a","e","i","e","y","o"].includes(ch[i].toLowerCase())) {
+        v++;
+    }
+
+}
+console.log( "number of words in the sentence is ",c+1," and number of vowels in the sentence is" ,v)
+
